00638cc8641fae38b7f27a7a60f563f48171cf9f
Majid Parvin <majid.parvin@comeon.com>
2019-11-23 22:07:29 +0100

    make mosaic routable

--- src/components/Movie/MovieMosaic/movie-mosaic.js
+++ src/components/Movie/MovieMosaic/movie-mosaic.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './movie-mosaic.scss';
 
-export default ({ title, overview, poster_path, imageBaseUrl }) => (
-  <div className="movie-mosaic">
-    <img src={`${imageBaseUrl}/w300${poster_path}`} alt={title} />
-    <footer>
-      <strong>{title}</strong>
-      <div>
-        <p className="overview">{overview}</p>
-      </div>
-    </footer>
-  </div>
+export default ({ title, overview, poster_path, imageBaseUrl, id }) => (
+  <a href={`/movie/${id}/${title}`}>
+    <div className="movie-mosaic">
+      <img src={`${imageBaseUrl}/w300${poster_path}`} alt={title} />
+      <footer>
+        <strong>{title}</strong>
+        <div>
+          <p className="overview">{overview}</p>
+        </div>
+      </footer>
+    </div>
+  </a>
 );
