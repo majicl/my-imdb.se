0cea0d15715f12fbbe92383260f35d4d7faac19a
Majid Parvin <majid.parvin@comeon.com>
2019-11-23 19:24:38 +0100

    add movie-provider config

---
+++ config/movie-provider.js
@@ -0,0 +1,6 @@
+
+export default {
+   baseUrl: process.env.MOVIE_BASE_URL || "","http://api.themoviedb.org/3",
+   apiKey:process.env.MOVIE_API_KEY || "",
+   imagesBaseUrl: process.env.IMAGES_BASE_URL || "https://image.tmdb.org/t/p"
+}
